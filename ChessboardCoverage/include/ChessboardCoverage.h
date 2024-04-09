49b8f240ec14ff96cc9588179bca1b8f46a54f5b
风 路 <3461571786@qq.com>
2024-04-09 10:17:10 +0800

    ui set

---
+++ ChessboardCoverage/include/ChessboardCoverage.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include <QtWidgets/QMainWindow>
+#include "ui_ChessboardCoverage.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class ChessboardCoverageClass; };
+QT_END_NAMESPACE
+
+class ChessboardCoverage : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    ChessboardCoverage(QWidget *parent = nullptr);
+    ~ChessboardCoverage();
+private:
+    void build_ui();
+    void build_connect();
+private:
+    Ui::ChessboardCoverageClass *ui;
+};
---
+++ ChessboardCoverage/src/ChessboardCoverage.cpp
@@ -0,0 +1,19 @@
+﻿#include "ChessboardCoverage.h"
+
+ChessboardCoverage::ChessboardCoverage(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::ChessboardCoverageClass())
+{
+    ui->setupUi(this);
+    build_ui();
+}
+
+ChessboardCoverage::~ChessboardCoverage()
+{
+    delete ui;
+}
+
+void ChessboardCoverage::build_ui()
+{
+    this->setWindowTitle("棋盘覆盖演示");
+}
