2f46027328db44779c4c4c12c718e1e8cf2e3a8d
风 路 <3461571786@qq.com>
2024-05-11 22:12:31 +0800

    跨平台移植

---
+++ build/ChessboardCoverage_autogen/include_Debug/PX6WNWAFJD/moc_ChessboardCoverage.cpp
@@ -0,0 +1,201 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'ChessboardCoverage.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.5.2)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../../../source/src/ChessboardCoverage.h"
+#include <QtCore/qmetatype.h>
+
+#if __has_include(<QtCore/qtmochelpers.h>)
+#include <QtCore/qtmochelpers.h>
+#else
+QT_BEGIN_MOC_NAMESPACE
+#endif
+
+
+#include <memory>
+
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'ChessboardCoverage.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.5.2. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+#ifndef Q_CONSTINIT
+#define Q_CONSTINIT
+#endif
+
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+QT_WARNING_DISABLE_GCC("-Wuseless-cast")
+namespace {
+
+#ifdef QT_MOC_HAS_STRINGDATA
+struct qt_meta_stringdata_CLASSChessboardCoverageENDCLASS_t {};
+static constexpr auto qt_meta_stringdata_CLASSChessboardCoverageENDCLASS = QtMocHelpers::stringData(
+    "ChessboardCoverage",
+    "pbtn_resetChessboard_clicked",
+    "",
+    "pbtn_CoverageOperate_clicked",
+    "pbtn_skipToInitial_clicked",
+    "pbtn_skipToFinal_clicked",
+    "pbtn_lastStep_clicked",
+    "pbtn_nextStep_clicked",
+    "pbtn_autoDisplay_clicked"
+);
+#else  // !QT_MOC_HAS_STRING_DATA
+struct qt_meta_stringdata_CLASSChessboardCoverageENDCLASS_t {
+    uint offsetsAndSizes[18];
+    char stringdata0[19];
+    char stringdata1[29];
+    char stringdata2[1];
+    char stringdata3[29];
+    char stringdata4[27];
+    char stringdata5[25];
+    char stringdata6[22];
+    char stringdata7[22];
+    char stringdata8[25];
+};
+#define QT_MOC_LITERAL(ofs, len) \
+    uint(sizeof(qt_meta_stringdata_CLASSChessboardCoverageENDCLASS_t::offsetsAndSizes) + ofs), len 
+Q_CONSTINIT static const qt_meta_stringdata_CLASSChessboardCoverageENDCLASS_t qt_meta_stringdata_CLASSChessboardCoverageENDCLASS = {
+    {
+        QT_MOC_LITERAL(0, 18),  // "ChessboardCoverage"
+        QT_MOC_LITERAL(19, 28),  // "pbtn_resetChessboard_clicked"
+        QT_MOC_LITERAL(48, 0),  // ""
+        QT_MOC_LITERAL(49, 28),  // "pbtn_CoverageOperate_clicked"
+        QT_MOC_LITERAL(78, 26),  // "pbtn_skipToInitial_clicked"
+        QT_MOC_LITERAL(105, 24),  // "pbtn_skipToFinal_clicked"
+        QT_MOC_LITERAL(130, 21),  // "pbtn_lastStep_clicked"
+        QT_MOC_LITERAL(152, 21),  // "pbtn_nextStep_clicked"
+        QT_MOC_LITERAL(174, 24)   // "pbtn_autoDisplay_clicked"
+    },
+    "ChessboardCoverage",
+    "pbtn_resetChessboard_clicked",
+    "",
+    "pbtn_CoverageOperate_clicked",
+    "pbtn_skipToInitial_clicked",
+    "pbtn_skipToFinal_clicked",
+    "pbtn_lastStep_clicked",
+    "pbtn_nextStep_clicked",
+    "pbtn_autoDisplay_clicked"
+};
+#undef QT_MOC_LITERAL
+#endif // !QT_MOC_HAS_STRING_DATA
+} // unnamed namespace
+
+Q_CONSTINIT static const uint qt_meta_data_CLASSChessboardCoverageENDCLASS[] = {
+
+ // content:
+      11,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       7,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,   56,    2, 0x08,    1 /* Private */,
+       3,    0,   57,    2, 0x08,    2 /* Private */,
+       4,    0,   58,    2, 0x08,    3 /* Private */,
+       5,    0,   59,    2, 0x08,    4 /* Private */,
+       6,    0,   60,    2, 0x08,    5 /* Private */,
+       7,    0,   61,    2, 0x08,    6 /* Private */,
+       8,    0,   62,    2, 0x08,    7 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+Q_CONSTINIT const QMetaObject ChessboardCoverage::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_CLASSChessboardCoverageENDCLASS.offsetsAndSizes,
+    qt_meta_data_CLASSChessboardCoverageENDCLASS,
+    qt_static_metacall,
+    nullptr,
+    qt_incomplete_metaTypeArray<qt_meta_stringdata_CLASSChessboardCoverageENDCLASS_t,
+        // Q_OBJECT / Q_GADGET
+        QtPrivate::TypeAndForceComplete<ChessboardCoverage, std::true_type>,
+        // method 'pbtn_resetChessboard_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'pbtn_CoverageOperate_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'pbtn_skipToInitial_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'pbtn_skipToFinal_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'pbtn_lastStep_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'pbtn_nextStep_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'pbtn_autoDisplay_clicked'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>
+    >,
+    nullptr
+} };
+
+void ChessboardCoverage::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<ChessboardCoverage *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->pbtn_resetChessboard_clicked(); break;
+        case 1: _t->pbtn_CoverageOperate_clicked(); break;
+        case 2: _t->pbtn_skipToInitial_clicked(); break;
+        case 3: _t->pbtn_skipToFinal_clicked(); break;
+        case 4: _t->pbtn_lastStep_clicked(); break;
+        case 5: _t->pbtn_nextStep_clicked(); break;
+        case 6: _t->pbtn_autoDisplay_clicked(); break;
+        default: ;
+        }
+    }
+    (void)_a;
+}
+
+const QMetaObject *ChessboardCoverage::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *ChessboardCoverage::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_CLASSChessboardCoverageENDCLASS.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int ChessboardCoverage::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 7)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 7;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 7)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 7;
+    }
+    return _id;
+}
+QT_WARNING_POP
